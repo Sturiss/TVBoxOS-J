5dc313f3937fc493e21e13e9fb3c6f32528c7a38
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-11-11 10:36:54 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -276,7 +276,7 @@ public class HomeActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 if (!useCacheConfig)
-                                    Toast.makeText(HomeActivity.this, "自定义jar加载成功", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(HomeActivity.this, "配置加载完成\欢迎使用Lion Movies", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         }, 50);
@@ -293,11 +293,11 @@ public class HomeActivity extends BaseActivity {
                         mHandler.post(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(HomeActivity.this, "jar加载失败", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(HomeActivity.this, "配置加载失败\n退出重新登录", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         });
-                    }
+                    }，
                 });
             }
             return;
