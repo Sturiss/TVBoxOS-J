442691cc365c429bb72c9c28577f6bc5635f034d
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-11-18 22:37:22 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -152,16 +152,33 @@ public class ModelSettingFragment extends BaseLazyFragment {
                 dialog.show();
             }
         });
+
+
+
+
         findViewById(R.id.llAbout).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FastClickCheckUtil.check(v);
-                // AboutDialog dialog = new AboutDialog(mActivity);
-                // dialog.show();
+                AboutDialog dialog = new AboutDialog(mActivity);
+                dialog.show();
 
+                //   UpdateDialog.checkUpdate(mActivity, false);
+            }
+        });
+        findViewById(R.id.llVersion).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FastClickCheckUtil.check(v);
                 UpdateDialog.checkUpdate(mActivity, false);
             }
         });
+
+
+
+
+
+
         findViewById(R.id.llWp).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -598,14 +615,14 @@ public class ModelSettingFragment extends BaseLazyFragment {
                 tvRecStyleText.setText(Hawk.get(HawkConfig.HOME_REC_STYLE, false) ? "是" : "否");
             }
         });
-        
+
         findViewById(R.id.llSearchTv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FastClickCheckUtil.check(view);
                 loadingSearchRemoteTvDialog = new SearchRemoteTvDialog(mActivity);
                 EventBus.getDefault().register(loadingSearchRemoteTvDialog);
-                loadingSearchRemoteTvDialog.setTip("搜索附近DweiTV");
+                loadingSearchRemoteTvDialog.setTip("搜索附近TV");
                 loadingSearchRemoteTvDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialogInterface) {
@@ -656,8 +673,8 @@ public class ModelSettingFragment extends BaseLazyFragment {
             }
         });
     }
-    
-    
+
+
     public static SearchRemoteTvDialog loadingSearchRemoteTvDialog;
     public static List<String> remoteTvHostList;
     public static boolean foundRemoteTv;
